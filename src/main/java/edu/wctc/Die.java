6dc6d74a5f559cda372885d3a70d5194a7136616
Stacy Read <sread@ivaalsolutions.com>
2023-04-09 16:49:51 -0500

    Can roll die in range of face values

--- src/main/java/edu/wctc/Die.java
+++ src/main/java/edu/wctc/Die.java
@@ -1,7 +1,15 @@
 package edu.wctc;
 
-public class Die {
-    public void roll() {
+import java.util.Random;
 
+public class Die {
+    private int faceValue;
+    private Random random = new Random();
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    public void roll() {
+        faceValue = random.nextInt(1, 7);
     }
 }
--- src/test/java/edu/wctc/DieTest.java
+++ src/test/java/edu/wctc/DieTest.java
@@ -2,11 +2,23 @@ package edu.wctc;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DieTest {
     @Test
     public void canRollDie() {
         Die die = new Die();
         die.roll();
     }
-    
+
+    @Test
+    public void rollValueInRange() {
+        Die die = new Die();
+        for (int i = 0; i < 100; i++) {
+            die.roll();
+            int faceValue = die.getFaceValue();
+            assertTrue(faceValue >= 1 && faceValue <= 6,
+                    String.format("Face value (%d) should be between 1 and 6", faceValue));
+        }
+    }
 }
